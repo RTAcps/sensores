d2458e6e56f183bf5020f154c3084d77f28f4380
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-04-27 17:45:01 -0300

    refactor: add home to app and change favicon

--- frontend/sensores/src/app/app.component.ts
+++ frontend/sensores/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './core/component/footer/footer.component';
 import { HeaderComponent } from './core/component/header/header.component';
+import { HomeComponent } from './feature/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- frontend/sensores/src/app/app.config.ts
+++ frontend/sensores/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+  ],
 };
