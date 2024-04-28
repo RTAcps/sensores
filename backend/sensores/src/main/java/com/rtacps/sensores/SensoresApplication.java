e9285c5a5dbeba07976ef8a6fac427a23c8d30b8
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-04-28 05:24:49 -0300

    chore: create a springboot project

---
+++ backend/sensores/src/main/java/com/rtacps/sensores/SensoresApplication.java
@@ -0,0 +1,13 @@
+package com.rtacps.sensores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoresApplication.class, args);
+	}
+
+}
